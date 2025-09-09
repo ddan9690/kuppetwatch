387e7977e139239b0bda83a384c24cb9aac46ec2
ddan9690 <dancanokeyo08@gmail.com>
2025-09-09 21:30:43 +0300

    footer modify

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-kuppet-green-dark text-kuppet-white py-6 mt-16">
     <div class="container mx-auto text-center space-y-2">
         <p>&copy; {{ date('Y') }} KUPPETWatch. All rights reserved.</p>
-        <p class="text-kuppet-yellow text-sm">Empowering teachers, fostering unity, and promoting progress.</p>
+        {{-- <p class="text-kuppet-yellow text-sm">Empowering teachers, fostering unity, and promoting progress.</p> --}}
     </div>
 </footer>
